bed125166825adc45f0f990cafa0af20133bd4c2
DUCHOCHOY Franck <franck.duchochoy@orange.fr>
2024-08-19 17:10:55 +0200

    first react

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+const App = () => {
+  const name = "Lorem";
+  return (
+    <>
+      <div className="mainDiv"></div>
+      <h1
+        style={{
+          color: "black",
+          backgroundColor: "lightgray",
+        }}
+      >
+        Lorem
+      </h1>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex nihil earum
+        corporis, minima libero voluptatum adipisci inventore maiores
+        praesentium corrupti. Libero eos delectus sed atque minima molestiae
+        voluptatibus nemo ratione.
+      </p>
+    </>
+  );
+};
+
+export default App;
